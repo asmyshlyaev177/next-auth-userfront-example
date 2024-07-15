d01dca38f1047f71c20740e26fbebfa6cb181e90
asmyshlyaev177 <asmyshlyaev177@gmail.com>
2024-07-15 14:12:43 +0400

    auth provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { UserfrontProvider } from "@userfront/next/client";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,7 +17,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+        <UserfrontProvider tenantId={process.env.NEXT_PUBLIC_USERFRONT_TENANT_ID ?? ""}>
+          {children}
+        </UserfrontProvider>
+          </body>
     </html>
   );
 }
